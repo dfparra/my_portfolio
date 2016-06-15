972679af41ec105e6c3a2dbd84f37f7d466f630a
Daniel <danielparra93@gmail.com>
2016-06-15 19:14:18 -0400

    Basic code for the four html pages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var server = express();
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
-server.use(express.static(__dirname+'public'));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(express.static(__dirname+'/public'));
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
